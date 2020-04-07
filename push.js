ef4c06b3793a9407aa9c44fc01167e760004023e
Воронин Сергей Андреевич <voronin_sa@bdo.ru>
2020-04-07 09:53:15 +0300

    fix[sw]: push handler

--- push.js
+++ push.js
@@ -7,6 +7,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:s.voronin@bdo.ru', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/epWN4OslQaQ:APA91bFoamh4254kaf8KH3N0EDiF-JEApat2nPaRjRoDbyWA4L5mHQLrx_n6NFibvv7YiwBg9XMWpMMYwNiGHT0kf-r9qyEm6zT8Uyosz7jCpo6WNVP9Bij1e98hg3igFq34SmzyW4sL", "expirationTime": null, "keys": { "p256dh": "BMpruk4EajnEYzNGjP9yujEvKxKTcSIbUYh2DQhrp3mXVcSGouyxnCkQZy1DswBA48IdpIyfiplhYBwPd1NMINs", "auth": "K0rZx4C8Vk5qOVeWkmHZqQ" } };
 
 push.sendNotification(sub, 'test message');
